bae518ec914f0b18438465f2994668ddeb48a714
Rober Quin <quinonesroberto92@gmail.com>
2024-11-08 17:47:52 -0600

    FormsModule y ngSubmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ContadorModule,HeroesModule],
+  imports: [RouterOutlet,ContadorModule,HeroesModule,DbzModule],
   templateUrl: 'app.component.html',
   styleUrl: './app.component.css'
 })
